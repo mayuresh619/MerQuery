459e7843b7026074ddbe1b1e8e9f30c42322d907
Mayuresh <mayuresh.vedak619@gmail.com>
2020-12-25 22:35:22 +0530

    Added theme to core

--- CoreWebApplication/Controllers/HomeController.cs
+++ CoreWebApplication/Controllers/HomeController.cs
@@ -1,11 +1,6 @@
-﻿using CoreWebApplication.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CoreWebApplication.Controllers
 {
@@ -23,15 +18,7 @@ namespace CoreWebApplication.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+
     }
 }
